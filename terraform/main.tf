059bd1047897b2fe339bc60f069f277a02a34eca
Jose Albanes <josedevops1978@gmail.com>
2026-08-09 13:49:29 -0300

    terraform: add persistent mysql volume

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,7 @@ resource "docker_network" "app_network" {
   name   = "gestion-usuarios-network"
   driver = "bridge"
 }
+
+resource "docker_volume" "mysql_data" {
+  name = "gestion-usuarios-mysql-data"
+}
